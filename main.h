cff4282fbbf8c1d0941ff669c6ca0ddcd8a69a8e
Elchin <elcinceferli@gmail.com>
2023-11-29 04:19:19 -0800

    printf

--- main.h
+++ main.h
@@ -9,8 +9,5 @@ int _print_pointer(void *ptr);
 int _print_unsigned(unsigned int n);
 int _print_octal(unsigned int n);
 int _print_hex(unsigned int n, int uppercase);
-<<<<<<< HEAD
 
-=======
->>>>>>> 5e9305a97f9759843197770166086a5c6b25f8b5
 #endif /* MAIN_H */

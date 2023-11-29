553b1394578afc4aee33ce0094dc388da2372188
Elchin <elcinceferli@gmail.com>
2023-11-29 04:16:00 -0800

    printf

--- _printf.c
+++ _printf.c
@@ -2,18 +2,13 @@
 #include <stdint.h>
 #include <unistd.h>
 #include "main.h"
-<<<<<<< HEAD
 
-=======
->>>>>>> 5e9305a97f9759843197770166086a5c6b25f8b5
 int _printf(const char *format, ...) {
     va_list args;
     int count = 0;
-    va_start(args, format);
-<<<<<<< HEAD
 
-=======
->>>>>>> 5e9305a97f9759843197770166086a5c6b25f8b5
+    va_start(args, format);
+
     while (format && *format) {
         if (*format == '%' && *(format + 1)) {
             format++;
@@ -21,39 +16,28 @@ int _printf(const char *format, ...) {
                 case 'c':
                     count += _putchar(va_arg(args, int));
                     break;
-                case 's':
-                    count += _puts(va_arg(args, char *));
+                case 's': {
+                    char *str = va_arg(args, char *);
+                    if (str == NULL) {
+                        count += _puts("(null)");
+                    } else {
+                        count += _puts(str);
+                    }
                     break;
+                }
                 case 'd':
                     count += _print_number(va_arg(args, int));
-                    break;
-<<<<<<< HEAD
-		case 'i':
-                    count += _print_number(va_arg(args, int));
-                    break;
-		case 'u':
-=======
-                case 'i':
-                    count += _print_number(va_arg(args, int));
-                    break;
-                case 'u':
->>>>>>> 5e9305a97f9759843197770166086a5c6b25f8b5
-                    count += _print_unsigned(va_arg(args, unsigned int));
                     break;
                 case 'p':
                     count += _print_pointer(va_arg(args, void *));
                     break;
-<<<<<<< HEAD
-		case 'o':
-                    count += _print_octal(va_arg(args, unsigned int));
+                case 'u':
+                    count += _print_unsigned(va_arg(args, unsigned int));
                     break;
-		case 'x':
-=======
                 case 'o':
                     count += _print_octal(va_arg(args, unsigned int));
                     break;
                 case 'x':
->>>>>>> 5e9305a97f9759843197770166086a5c6b25f8b5
                     count += _print_hex(va_arg(args, unsigned int), 0);
                     break;
                 case 'X':
@@ -71,20 +55,15 @@ int _printf(const char *format, ...) {
         }
         format++;
     }
+
     va_end(args);
+
     return count;
 }
-<<<<<<< HEAD
 
 int _putchar(char c) {
     return write(1, &c, 1);
 }
-
-=======
-int _putchar(char c) {
-    return write(1, &c, 1);
-}
->>>>>>> 5e9305a97f9759843197770166086a5c6b25f8b5
 int _puts(char *str) {
     int count = 0;
     while (str && *str) {
@@ -93,34 +72,11 @@ int _puts(char *str) {
     }
     return count;
 }
-<<<<<<< HEAD
-
-=======
->>>>>>> 5e9305a97f9759843197770166086a5c6b25f8b5
 int _print_number(int n) {
     if (n < 0) {
         _putchar('-');
         n = -n;
     }
-<<<<<<< HEAD
-
-    if (n / 10 != 0) {
-        _print_number(n / 10);
-    }
-
-    return _putchar((n % 10) + '0');
-}
-
-int _print_pointer(void *ptr) {
-    int count = 0, i;
-
-    uintptr_t addr = (uintptr_t)ptr;
-
-    int nibbles = (sizeof(void *) * 2);
-
-    count += _putchar('0') + _putchar('x');
-
-=======
     if (n / 10 != 0) {
         _print_number(n / 10);
     }
@@ -131,7 +87,6 @@ int _print_pointer(void *ptr) {
     uintptr_t addr = (uintptr_t)ptr;
     int nibbles = (sizeof(void *) * 2);
     count += _putchar('0') + _putchar('x');
->>>>>>> 5e9305a97f9759843197770166086a5c6b25f8b5
     /* Print each nibble without leading zeros */
     for (i = nibbles - 1; i >= 0; i--) {
         int digit = (addr >> (i * 4)) & 0xF;
@@ -139,48 +94,18 @@ int _print_pointer(void *ptr) {
             count += _putchar("0123456789abcdef"[digit]);
         }
     }
-<<<<<<< HEAD
-
     return count;
 }
-
-=======
-    return count;
-}
->>>>>>> 5e9305a97f9759843197770166086a5c6b25f8b5
 int _print_unsigned(unsigned int n) {
     if (n / 10 != 0) {
         _print_unsigned(n / 10);
     }
-<<<<<<< HEAD
-
     return _putchar((n % 10) + '0');
 }
-
-=======
-    return _putchar((n % 10) + '0');
-}
->>>>>>> 5e9305a97f9759843197770166086a5c6b25f8b5
 int _print_octal(unsigned int n) {
     if (n / 8 != 0) {
         _print_octal(n / 8);
     }
-<<<<<<< HEAD
-
-    return _putchar((n % 8) + '0');
-}
-
-int _print_hex(unsigned int n, int uppercase) {
-    int digit = n % 16;
-    char hexChar = (digit < 10) ? ('0' + digit) : (uppercase ? ('A' + digit - 10) : ('a' + digit - 10));
-
-    if (n / 16 != 0) {
-        _print_hex(n / 16, uppercase);
-    }
-
-    return _putchar(hexChar);
-}
-=======
     return _putchar((n % 8) + '0');
 }
 int _print_hex(unsigned int n, int uppercase) {
@@ -191,11 +116,3 @@ int _print_hex(unsigned int n, int uppercase) {
     }
     return _putchar(hexChar);
 }
-
-
-
-
-
-
-
->>>>>>> 5e9305a97f9759843197770166086a5c6b25f8b5
